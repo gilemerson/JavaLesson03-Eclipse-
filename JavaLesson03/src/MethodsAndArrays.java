56ecd3e7446711dccd16c1321656e26c038f95a3
Gil Emerson <emerson-gil@outlook.com>
2016-02-24 15:29:02 -0500

    Added Drive Class to project

---
+++ JavaLesson03/src/MethodsAndArrays.java
@@ -0,0 +1,16 @@
+
+public class MethodsAndArrays {
+
+	public static void main(String[] args) {
+		//Declare two objects
+		long myLongVariable = 100000L;
+		int myIntVariable;
+		
+		myIntVariable = (int) myLongVariable;
+		
+		System.out.println(myIntVariable);
+		
+
+	}
+
+}
