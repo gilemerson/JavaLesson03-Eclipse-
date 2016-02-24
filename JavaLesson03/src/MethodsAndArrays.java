3848c8afe54a5115af5bb81cf47a97c2b08a4c75
Gil Emerson <emerson-gil@outlook.com>
2016-02-24 16:48:30 -0500

    Added Roll Rice Method

--- JavaLesson03/src/MethodsAndArrays.java
+++ JavaLesson03/src/MethodsAndArrays.java
@@ -1,16 +1,28 @@
+import java.security.SecureRandom;
 
 public class MethodsAndArrays {
 
 	public static void main(String[] args) {
-		//Declare two objects
-		long myLongVariable = 100000L;
-		int myIntVariable;
 		
-		myIntVariable = (int) myLongVariable;
-		
-		System.out.println(myIntVariable);
-		
+		System.out.println(rollDice(3, 6));	
 
+	}
+		
+	//Method returns an int = result;
+	public static int rollDice(int numDice, int numSides ){
+		//Random Numbers
+		SecureRandom generateRandom = new SecureRandom();
+		
+		int result = 0;
+		//Array of integers
+		int[] Dice = new int [numDice];
+		
+		//ForLoop
+		for(int index=0; index < numDice-1; index++){
+			result += generateRandom.nextInt(6) +1;;
+		}
+	
+		return result;
 	}
 
 }
